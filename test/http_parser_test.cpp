9d2c99bec0f7b08c012620fe96e7d62db27468b9
hongliuliao <lyyseek@gmail.com>
2014-10-30 17:12:53 +0800

    add test

---
+++ test/http_parser_test.cpp
@@ -0,0 +1,16 @@
+/*
+ * http_parser_test.cpp
+ *
+ *  Created on: Oct 30, 2014
+ *      Author: liao
+ */
+#include "http_parser.h"
+#include "simple_log.h"
+
+int main () {
+	std::string temp = "  aaa:bbb\r\n";
+	std::vector<std::string> parts = split_str(temp, ':');
+	LOG_DEBUG("parts[0]:%s", parts[0].c_str())
+	LOG_DEBUG("parts[1]:%s", parts[1].c_str())
+	return 0;
+}
